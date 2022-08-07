864cb1dc696135564f9cd3fa3f4c77cd2d78a7f5
Nam Tran <namtran0110@gmail.com>
2022-08-07 23:53:07 +0000

    new update

--- new_project.R
+++ new_project.R
@@ -7,3 +7,14 @@ Vector2
 #List is recursive
 list1 <- list(1,"be",FALSE, vector_3 <- c("Sun",14,TRUE), list2 <- list(1,2))
 list1
+
+# Lubridate package
+install.packages("tidyverse") 
+library(tidyverse)
+library(lubridate)
+
+today()
+as_date(now())
+mdy("January 20th, 2021")
+ymd(20210120)
+ymd_hms("2021-01-20 20:11:59")
