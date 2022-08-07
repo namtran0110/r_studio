21ff0da22a34112a90a3b95942e2fb5a12498a02
Nam Tran <namtran0110@gmail.com>
2022-08-07 22:51:31 +0000

    addd new_project.R

---
+++ new_project.R
@@ -0,0 +1,9 @@
+#Vector is atomic, will force data into one type
+vector1 <- c(FALSE,100,"L")
+vector1
+Vector2 <- c(1,FALSE)
+Vector2
+
+#List is recursive
+list1 <- list(1,"be",FALSE, vector_3 <- c("Sun",14,TRUE), list2 <- list(1,2))
+list1
